c9a70b61fc66ce4e71f4e4317c693f26b7d3a8fb
bozinovskia <andrejbozinovski21@gmail.com>
2024-12-12 01:57:19 +0100

    Lab3

--- src/main/java/mk/ukim/finki/wp/lab/bootstrap/DataHolder.java
+++ src/main/java/mk/ukim/finki/wp/lab/bootstrap/DataHolder.java
@@ -28,31 +28,31 @@ public class DataHolder {
     @PostConstruct
     public void init() {
         // Add Artists
-        artistRepository.save(new Artist(1L, "Nirvana", "", "Legendary grunge pioneers"));
-        artistRepository.save(new Artist(2L, "Queen", "", "Iconic rock legends"));
-        artistRepository.save(new Artist(3L, "Pink", "Floyd", "Psychedelic rock innovators"));
-        artistRepository.save(new Artist(4L, "Pearl", "Jam", "Seattle grunge icons"));
-        artistRepository.save(new Artist(5L, "Muse", "", "Alternative rock band"));
-        artistRepository.save(new Artist(6L, "Soundgarden", "", "Pioneers of grunge"));
+        artistRepository.save(new Artist(1L, "Avicii", "", "Legendary EDM producer"));
+        artistRepository.save(new Artist(2L, "Calvin", "Harris", "Global EDM superstar"));
+        artistRepository.save(new Artist(3L, "Zedd", "", "Grammy-winning EDM artist"));
+        artistRepository.save(new Artist(4L, "Martin", "Garrix", "Young EDM prodigy"));
+        artistRepository.save(new Artist(5L, "Deadmau5", "", "Innovative electronic artist"));
+        artistRepository.save(new Artist(6L, "The Chainsmokers", "", "Chart-topping EDM duo"));
 
         // Add Albums
-        albumRepository.save(new Album(1L, "Nevermind", "Grunge", "1991", new ArrayList<>()));
-        albumRepository.save(new Album(2L, "A Night at the Opera", "Rock", "1975", new ArrayList<>()));
-        albumRepository.save(new Album(3L, "The Dark Side of the Moon", "Progressive Rock", "1973", new ArrayList<>()));
-        albumRepository.save(new Album(4L, "Ten", "Grunge", "1991", new ArrayList<>()));
-        albumRepository.save(new Album(5L, "Black Holes and Revelations", "Alternative Rock", "2006", new ArrayList<>()));
+        albumRepository.save(new Album(1L, "True", "EDM", "2013", new ArrayList<>()));
+        albumRepository.save(new Album(2L, "18 Months", "EDM", "2012", new ArrayList<>()));
+        albumRepository.save(new Album(3L, "Clarity", "EDM", "2012", new ArrayList<>()));
+        albumRepository.save(new Album(4L, "Animals", "EDM", "2013", new ArrayList<>()));
+        albumRepository.save(new Album(5L, "Random Album Title", "EDM", "2008", new ArrayList<>()));
 
         // Add Songs
-        Album nevermind = albumRepository.findById(1L).orElse(null);
-        Album nightAtTheOpera = albumRepository.findById(2L).orElse(null);
-        Album darkSide = albumRepository.findById(3L).orElse(null);
-        Album ten = albumRepository.findById(4L).orElse(null);
-        Album blackHoles = albumRepository.findById(5L).orElse(null);
+        Album trueAlbum = albumRepository.findById(1L).orElse(null);
+        Album eighteenMonths = albumRepository.findById(2L).orElse(null);
+        Album clarity = albumRepository.findById(3L).orElse(null);
+        Album animals = albumRepository.findById(4L).orElse(null);
+        Album randomAlbumTitle = albumRepository.findById(5L).orElse(null);
 
-        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "1", "Smells Like Teen Spirit", "Grunge", 1991, nevermind, new ArrayList<>(), new ArrayList<>()));
-        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "2", "Bohemian Rhapsody", "Rock", 1975, nightAtTheOpera, new ArrayList<>(), new ArrayList<>()));
-        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "3", "Time", "Progressive Rock", 1973, darkSide, new ArrayList<>(), new ArrayList<>()));
-        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "4", "Alive", "Grunge", 1991, ten, new ArrayList<>(), new ArrayList<>()));
-        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "5", "Starlight", "Alternative Rock", 2006, blackHoles, new ArrayList<>(), new ArrayList<>()));
+        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "1", "Wake Me Up", "EDM", 2013, trueAlbum, new ArrayList<>(), new ArrayList<>()));
+        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "2", "Feel So Close", "EDM", 2012, eighteenMonths, new ArrayList<>(), new ArrayList<>()));
+        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "3", "Clarity", "EDM", 2012, clarity, new ArrayList<>(), new ArrayList<>()));
+        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "4", "Animals", "EDM", 2013, animals, new ArrayList<>(), new ArrayList<>()));
+        songRepository.save(new Song(songIdGenerator.getAndIncrement(), "5", "Strobe", "EDM", 2008, randomAlbumTitle, new ArrayList<>(), new ArrayList<>()));
     }
 }
